51c48160b6f58fbdb44ce9b453ca04a41fe52391
Nic Schlueter <schlueter@gmail.com>
2011-04-17 18:00:42 -0400

    Initial Commit

---
+++ Pow Link/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  Pow Link
+#
+#  Created by Nicholas Schlueter on 4/17/11.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
---
+++ Pow Link/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Pow Link
+//
+//  Created by Nicholas Schlueter on 4/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
